4a47cf7acb0737a86d0dbde782bca9b3b6b75f26
Karan Goel <karan@goel.im>
2014-01-18 23:05:05 -0800

    multiple markers

--- public/js/initialize.js
+++ public/js/initialize.js
@@ -8,7 +8,38 @@ function initialize() {
     body.innerHTML="Geolocation is not supported by this browser.";
   }
 }
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
 function showOnMap(position) {
+
+  var pinColor = "EEEEEE";
+  var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0,0),
+    new google.maps.Point(10, 34));
+  var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+    new google.maps.Size(40, 37),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(12, 35));
+
+
   var latitude = position.coords.latitude;
   var longitude = position.coords.longitude;
   var myLatlng = new google.maps.LatLng(latitude, longitude);
@@ -17,47 +48,50 @@ function showOnMap(position) {
     center: myLatlng,
     zoom: 17
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
+  var map = new google.maps.Map(document.getElementById("map_canvas"),
       mapOptions);
   var currentPosition = new google.maps.Marker({
     position: myLatlng,
     map: map,
-    title:"You are here!"
+    title:"You are here!",
+    icon: pinImage,
+    shadow: pinShadow
   });
-  currentPosition.setMap(map);
+  console.log(myLatlng);
+  //currentPosition.setMap(map);
 
   $.get("/get/bathrooms/",function(data,status){
     //JSON.parse(data);
-    for (var i = 0; i < data.bathrooms.length; i++) {
-      if (data.bathrooms[i]["loc"] != undefined) {
-        console.log(data.bathrooms[i]);
-        var lat = data.bathrooms[i]["loc"].lat;
-        var lng = data.bathrooms[i]["loc"].lng;
-        /*var queryURL = "http://maps.googleapis.com/maps/api/distancematrix/json?"+
-            "origins="+latitude+","+longitude+
-            "&destinations="+lat+","+lng+
-            "&mode="+"walking"+
-            "&sensor="+"false"
-        console.log(queryURL);
-        $.get(queryURL, function(data,status){
-            JSON.parse(data);
-            if (data.status == "OK") {
-              var distance = data.rows.elements.distance.value;*/
-              var distance = Math.sqrt(Math.abs(Math.pow((lat - latitude),2) - Math.pow((lng - longitude),2)));
-              console.log(distance);
-              if (distance <= 500) {
-                var restRoom = new google.maps.LatLng(lat, lng);
-                  var nearby = new google.maps.Marker({
-                    position: restRoom,
-                    map: map,
-                    title:"REST ROOM!"
-                  });
-                  nearby.setMap(map);
-                  console.log("SET!!");
-              }
-            //}
-          //});
-        //}
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (var db_loc = 0; db_loc < data.bathrooms.length; db_loc++) {
+      if (data.bathrooms[db_loc]["loc"] != undefined) {
+        var lat = data.bathrooms[db_loc]["loc"].lat;
+        var lng = data.bathrooms[db_loc]["loc"].lng;
+
+        var distance = getDistanceFromLatLonInKm(latitude, longitude, lat, lng) * 1000;
+
+        console.log(distance);
+        if (distance <= 500) {
+          var restRoom = new google.maps.LatLng(lat, lng);
+          marker = new google.maps.Marker({
+            position: restRoom,
+            map: map
+            //title: data.bathrooms[i].name
+          });
+          console.log(restRoom);
+
+        google.maps.event.addListener(marker, 'click', (function(marker, db_loc) {
+          return function() {
+            //infowindow.setContent(locations[db_loc][0]);
+            infowindow.open(map, marker);
+          }
+        })(marker, db_loc));
+          //nearby.setMap(map);
+        }
       }
     }
   });
