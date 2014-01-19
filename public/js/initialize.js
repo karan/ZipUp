15d6dc16e265cb3faa2495dee0e616d304508b99
Jiaming927 <masterjm@cs.washington.edu>
2014-01-18 19:22:54 -0800

    Feature adding
    Finding the rest rooms around in 500m radius

--- public/js/initialize.js
+++ public/js/initialize.js
@@ -23,5 +23,36 @@ function showOnMap(position) {
     title:"You are here!"
   });
   currentPosition.setMap(map);
+
+	$.get("demo_test.asp",function(data,status){
+    if (status == 200) {
+    	JSON.parse(data);
+   		for (var i = 0; i < data.bathroom.length; i++) {
+   			var lat = data.bathroom[i].lat;
+   			var lng = data.bathroom[i].lng;
+   			$.post("http://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&sensor=false",
+  				{
+				    origins: latitude + "," + longitude,
+				    destinations: lat+ "," + lng
+				    mode: "walking"
+				  },
+				  function(data,status){
+				    JSON.parse(data);
+				    if (data.status == "OK") {
+				    	var distance = data.rows.elements.distance.value;
+				    	if (distance <= 500) {
+				    		var restRoom = new google.maps.LatLng(lat, lng);
+				    		  var currentPosition = new google.maps.Marker({
+								    position: restRoom
+								    map: map,
+									  title:"REST ROOM!"
+								  });
+								  currentPosition.setMap(map);
+				    	}
+				    }
+				  });
+   		}
+    }
+  });
 }
 google.maps.event.addDomListener(window, 'load', initialize);google.maps.event.addDomListener(window, 'load', initialize);
