8400ea2389bc4d2d741c89a247a5d3c4834a461a
Jiaming927 <masterjm@cs.washington.edu>
2014-01-18 16:25:07 -0800

    Separate JS file

---
+++ public/js/initialize.js
@@ -0,0 +1,26 @@
+function initialize() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showOnMap);
+  }
+  else{
+    body.innerHTML="Geolocation is not supported by this browser.";
+  }
+}
+function showOnMap(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var myLatlng = new google.maps.LatLng(latitude, longitude);
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 15
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+  var currentPosition = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"You are here!"
+  });
+  currentPosition.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);google.maps.event.addDomListener(window, 'load', initialize);
