2b599567f16232ab3973d6687e9b89243c08b1be
Karan Goel <karan@goel.im>
2014-03-27 16:02:36 -0700

    add remote dp and deploy to heroku

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-var dbURI = 'mongodb://localhost/zipup';
+var dbURI = 'mongodb://heroku_app23454617:c6igkhqe6ojpv02um4v0v812ut@ds035617.mongolab.com:35617/heroku_app23454617';
 mongoose.connect(dbURI);
 
 // when connected with db
